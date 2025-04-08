c7c65f1aee2909c1737289ffa95dc9124c1dc7df
pmk456 <patanmusthakheem786@gmail.com>
2025-04-08 22:12:37 +0530

    Removed Exception Raising

--- app/db_handler.py
+++ app/db_handler.py
@@ -22,7 +22,7 @@ class DBHandler:
             self.collection.insert_one(data)
             return True
         except:
-            raise
+            return False
         
 if __name__ == "__main__":
     handler = DBHandler()
