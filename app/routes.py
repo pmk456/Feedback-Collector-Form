3c41717cc4864ffc1f5264c64aa44364f2c297ad
pmk456 <patanmusthakheem786@gmail.com>
2025-04-09 10:38:11 +0530

    Solved a bug which is causing internal errors

--- app/routes.py
+++ app/routes.py
@@ -38,13 +38,15 @@ def api_feedback():
         message = "Database Connection Error!"
         code = 400
         success = False
-    db_status = mongo_handler.insert_record(
-            {'Name': data['name'],
-            'Email': data['email'],
-            'Message': data['msg'],
-            'Time Stamp': datetime.datetime.now()})
-    if not db_status:
-        message = "Database Insertion Error!"
-        code = 400
-        success = False
+    else:
+        db_status = mongo_handler.insert_record(
+                {'Name': data['name'],
+                'Email': data['email'],
+                'Type': data['type'],
+                'Message': data['msg'],
+                'Time Stamp': datetime.datetime.now()})
+        if not db_status:
+            message = "Database Insertion Error!"
+            code = 400
+            success = False
     return jsonify({'success': success, 'message': message}), code
